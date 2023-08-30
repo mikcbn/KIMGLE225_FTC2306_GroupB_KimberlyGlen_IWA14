58f917c4e53e204b0628b0bb4ff101f78335c202
mikcbn <136186783+mikcbn@users.noreply.github.com>
2023-08-30 09:21:57 +0200

    Added notes

--- challenge-1/script.js
+++ challenge-1/script.js
@@ -18,5 +18,28 @@ sayHello();
 -The error "hobby is not a function" occurs because hobby has been declared as a
 variable initially and then as a function. 
 
--
+-Issues:
+
+  -The function definitions for add and multiply were incorrect. The arrow
+   function syntax was not used correctly. 
+  -The add function is defined to add two numbers, but it subtracts them instead. 
+  -The multiply function is defined to subtract two numbers, which is incorrect. 
+  -The internal function references undefined properties this.add, this.a, and this.b. 
+  -In the internal function, the multiplication result of this.multiply is calculated 
+   but not stored or used. 
+  -The internal function does not return the intended result; it returns
+   the this context instead.
 */
+
+/**
+ * Fixes Made and Explanations:
+
+    *Arrow function syntax is used correctly for the add and multiply functions.
+    *The add function performs addition, and the multiply function performs multiplication. 
+    *The internal function is defined correctly, and it uses the
+     properties this.internal.a and this.internal.b to perform calculations. 
+    *The added and multiplied results are stored and returned as an object from the
+     internal function. 
+    *In the example1.calculate() and example2.calculate() invocations, 
+     the results are logged to the console using console.log.
+ */
--- challenge-2/script.js
+++ challenge-2/script.js
@@ -33,3 +33,23 @@ const example2 = {
 
 example1.calculate();
 example2.calculate();
+
+
+/*
+ * Inconsistent variable naming: The variables are defined with different
+   names (firstName, age, hobby), but they are referenced with different
+   names (name, parameter) later in the code. Improper use of console
+   function: console is used incorrectly as console(parameter) instead of
+   console.log(parameter). The function hobby has the same name as the
+   variable hobby.
+
+Fixes:
+  Use consistent variable names (e.g., firstName, age, hobby).
+  Change console(parameter) to console.log(parameter) or a similar valid log statement.
+  Rename the function hobby to something else, so it doesn't conflict with the variable hobby.
+  Variables are declared using const to ensure they cannot be re-assigned accidentally.
+  The function logTwice now takes a message parameter, and console.log(message) is used to log the message twice. 
+  The function sayHello is defined and uses template literals to create the message variable containing the formatted greeting.
+  The function sayHello calls the logTwice function with the message parameter to print the greeting twice.
+  The function sayHello is invoked at the end with sayHello(); to execute the code.
+ */
